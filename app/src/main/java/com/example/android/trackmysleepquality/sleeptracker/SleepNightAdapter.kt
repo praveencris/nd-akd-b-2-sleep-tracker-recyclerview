e6eceeb74b575829cb280cb1adee939d1c6250bc
Praveen <pkchmits@gmail.com>
2020-12-25 10:17:44 +0530

    Completed exercise

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,15 +16,34 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
-// TODO (02) Create SleepNightAdapter class and extend it
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.database.SleepNight
+
+class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
+    val data = listOf<SleepNight>()
+
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item: SleepNight = data.get(position)
+        holder.textView.text = item.sleepQuality.toString()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        TODO("Not yet implemented")
+    }
+}
+// DONE (02) Create SleepNightAdapter class and extend it
 // from RecyclerView.Adapter<TextItemViewHolder>
 
-// TODO (03) Create a variable, data, that holds a list of SleepNight.
+// DONE (03) Create a variable, data, that holds a list of SleepNight.
 
-// TODO (04) Override getItemCount() to return the total number of items in the data set.
+// DONE (04) Override getItemCount() to return the total number of items in the data set.
 
-// TODO (05) Override onBindViewHolder() and have it update the contents of the
+// DONE (05) Override onBindViewHolder() and have it update the contents of the
 // ViewHolder to reflect the item at the given position.
 
-// TODO (06) Override onCreateViewHolder(). We'll complete this method
+// DONE (06) Override onCreateViewHolder(). We'll complete this method
 // in a later exercise.
